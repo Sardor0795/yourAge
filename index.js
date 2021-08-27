a0d1377a639f58761c908fa2339df22a41ac672c
Sardor0795 <english.programmer.9595@gmail.com>
2021-08-27 11:33:57 +0500

    My first JS code

---
+++ index.js
@@ -0,0 +1,5 @@
+let myAge = Number(prompt("Tug'ilgan yilingizni kiriting:"));
+let age = 2021 - myAge;
+alert("Sizning yoshingiz " + age + " da");
+let yourName = String(prompt("Ismingizni kiriting:"));
+alert(`Salom ${yourName}. Bizning saxifaga xush kelibsiz!`)
